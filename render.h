8949a1d2a610f892b585b53d50708933ba1b5f87
tadzio <tadzio.holtrop@icloud.com>
2025-02-20 23:40:15 +0000

    add defocus blur

--- render.h
+++ render.h
@@ -15,8 +15,14 @@ class camera {
         int image_width;
         int samples_per_pixel = 1;
         int max_depth = 2;
-        point3 center = point3(0, 0, 0);    // camera center point
-        double focal_length = 1.0;
+        
+        double vfov = 20;                   // vertical field of view
+        point3 lookfrom = point3(1,1,3);    // point camera is looking from
+        point3 lookat = point3(0,0,-1);     // point camera is looking at
+        vec3 vup = vec3(0,1,0);             // camera-relative up direction
+
+        double defocus_angle = 0;
+        double focus_dist = 1;
 
         camera(): aspect_ratio(1.0), image_width(100) {
             initialize();
@@ -62,7 +68,7 @@ class camera {
                                 + ((i + offset.x()) * pixel_delta_u)
                                 + ((j + offset.y()) * pixel_delta_v);
             
-            auto ray_origin = center;
+            auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
             auto ray_direction = pixel_sample - ray_origin;
 
             return ray(ray_origin, ray_direction);
@@ -74,12 +80,22 @@ class camera {
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
+        point3 defocus_disk_sample() const {
+            // returns a random point in the camera defocus disk
+            auto p = random_in_unit_disk();
+            return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+        }
+
     private:
         int image_height;           // rendered image height
         double pixel_samples_scale; // scaling factor for rgb values
+        point3 center;              // camera center
         point3 pixel00_loc;         // location of pixel (0, 0) 
         vec3 pixel_delta_u;         // pixel spacing in horizontal
         vec3 pixel_delta_v;         // pixel spacing in vertical direction
+        vec3 u, v, w;               // camera frame basis vectors
+        vec3 defocus_disk_u;        // defocus disk horizontal radius
+        vec3 defocus_disk_v;        // defocus deisk vertical radius
 
         void initialize() {
             image_height = int(image_width / aspect_ratio);
@@ -88,18 +104,33 @@ class camera {
             // calculate scaling factor for rgb values
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
+            center = lookfrom;
+
             // viewport parameters
-            auto viewport_height = 2.0;
+            auto theta = degrees_to_radians(vfov);
+            auto viewport_height = 2 * std::tan(theta/2) * focus_dist;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            
+            // calculate u, v, w basis vectors
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
+            // calculate vectros across horizontal and down vertical viewport edges
+            auto viewport_u = viewport_width * u;       // across horizontal viewport edge
+            auto viewport_v = viewport_height * -v;     // down view vertical edge
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
             auto viewport_upper_left = center 
-                                        - vec3(0, 0, focal_length)
+                                        - (focus_dist * w)
                                         - viewport_u / 2
                                         - viewport_v / 2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+            // Calculate the camera defocus disk basis vectors.
+            auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+            defocus_disk_u = u * defocus_radius;
+            defocus_disk_v = v * defocus_radius;
         }
 
         color ray_color(const ray& r, int depth, const hittable& world) const {
