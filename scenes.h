e982e9cf21ad427d7f50cdfceb042897e6240579
tadzio <tadzio.holtrop@icloud.com>
2025-03-22 22:42:21 +0000

    add y-axis rotation class for primitives

--- scenes.h
+++ scenes.h
@@ -17,12 +17,15 @@ void sandbox(camera& cam, hittable_list& world) {
     auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     auto material_right  = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
     
+    shared_ptr<hittable> sphere_center = make_shared<sphere>(point3( 0.0,    0.0, -2.0),   0.5, material_center);
+    sphere_center = make_shared<translate>(sphere_center, vec3(0,0,0));
+    
     world.add(make_shared<sphere>(point3( 0.0, -1000.5, -1.0), 1000.0, material_ground));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -8.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -2.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(sphere_center);
     // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.4, material_bubble));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-    world.add(make_shared<disk>  (point3( 0.0,    2.0,  -1.0), vec3(1.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0), difflight));
+    world.add(make_shared<disk>  (point3( 0.0,    2.0,  -1.0), vec3(5.0, 0.0, 0.0), vec3(0.0, 0.0, 5.0), difflight));
 
     cam.aspect_ratio =  16.0 / 9.0;
     cam.lookat = point3(0, 0, 0);
@@ -43,9 +46,17 @@ void cornell_box(camera& cam, hittable_list& world) {
     world.add(make_shared<quad>(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white));
     world.add(make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
     world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
+    
+    // boxes inside the cornell box
+    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    world.add(box1);
 
-    world.add(box(point3(130, 0, 65), point3(295, 165, 230), white));
-    world.add(box(point3(265, 0, 295), point3(430, 330, 460), white));
+    shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    world.add(box2);
 
     cam.aspect_ratio      = 1.0;
     cam.image_width       = 600;
