5116d0a364749bd7a6ce1585544fc870acb1cbde
tadzio <tadzio.holtrop@icloud.com>
2025-05-07 14:29:15 +0100

    add mixture densities

--- render.h
+++ render.h
@@ -294,9 +294,12 @@ class camera {
                     break;
                 }
 
-                hittable_pdf light_pdf(lights, rec.p);
-                scattered = ray(rec.p, light_pdf.generate());             
-                pdf_value = light_pdf.value(scattered.direction());
+                auto p0 = make_shared<hittable_pdf>(lights, rec.p);
+                auto p1 = make_shared<cosine_pdf>(rec.normal);
+                mixture_pdf mixed_pdf(p0, p1);
+
+                scattered = ray(rec.p, mixed_pdf.generate());             
+                pdf_value = mixed_pdf.value(scattered.direction());
         
                 double scattering_pdf = rec.mat->scattering_pdf(current_ray, rec, scattered);
         
